1a45b9ef89bf914a631f9c99485ea288a69f1929
Debotos Das <debotosdas@gmail.com>
2019-07-11 18:38:15 +0600

    single file upload working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+
+import gql from 'graphql-tag'
+import { Mutation } from 'react-apollo'
+
+const uploadImage = gql`
+	mutation($file: Upload!) {
+		changeProfileImage(image: $file) {
+			secure_url
+		}
+	}
+`
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Mutation mutation={uploadImage}>
+			{mutate => (
+				<input type="file" onChange={e => mutate({ variables: { file: e.target.files[0] } })} />
+			)}
+		</Mutation>
+	)
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { ApolloClient } from 'apollo-client'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { createUploadLink } from 'apollo-upload-client'
+import { ApolloProvider } from 'react-apollo'
+
+const client = new ApolloClient({
+	cache: new InMemoryCache(),
+	link: createUploadLink({
+		uri: 'http://localhost:5001/graphql',
+		headers: {
+			'x-token':
+				'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjRlOGVmODdkLTdmZDEtNDBjNC04ZTgyLTlkMTQwYTRiOTFhYyIsImVtYWlsIjoiZGVib3Rvc2Rhc0BnbWFpbC5jb20iLCJ1c2VybmFtZSI6ImRlYm90b3MiLCJyb2xlIjoiQURNSU4iLCJpYXQiOjE1NjI4NDQ5MDksImV4cCI6MTU2Mjg0ODUwOX0.FaC46Rx9ATzWdNSQ-uAZ0lZLHifQnGHYYoTSuMF7cqY'
+		}
+	})
+})
+
+ReactDOM.render(
+	<ApolloProvider client={client}>
+		<App />
+	</ApolloProvider>,
+	document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
