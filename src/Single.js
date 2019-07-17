1fe71db631ebb2ec75e6b26f4e3bd028e1850436
Debotos Das <debotosdas@gmail.com>
2019-07-17 12:18:05 +0600

    Testing single product upload

--- src/Single.js
+++ src/Single.js
@@ -4,17 +4,28 @@ import gql from 'graphql-tag'
 import { Mutation } from 'react-apollo'
 
 const uploadImage = gql`
-	mutation($file: Upload!) {
-		changeProfileImage(image: $file) {
+	mutation($id: ID!, $file: Upload!) {
+		addProductImage(id: $id, image: $file) {
 			secure_url
 		}
 	}
 `
 
 export default () => (
-	<Mutation mutation={uploadImage}>
-		{mutate => (
-			<input type="file" onChange={e => mutate({ variables: { file: e.target.files[0] } })} />
-		)}
+	<Mutation mutation={uploadImage} onError={error => console.log(error.message)}>
+		{(mutate, { loading, data }) => {
+			if (loading) return <h6>Uploading...</h6>
+			if (data) return <h6>{data.addProductImage.secure_url}</h6>
+			return (
+				<input
+					type="file"
+					onChange={e =>
+						mutate({
+							variables: { file: e.target.files[0], id: 'f13b31b7-7a09-45bd-b747-052071c24b93' }
+						})
+					}
+				/>
+			)
+		}}
 	</Mutation>
 )
